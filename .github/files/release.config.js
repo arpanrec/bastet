b1f5f8a4be92455f005834907b68696011feb83f
arpan <arpan.rec@gmail.com>
2024-01-21 02:23:03 +0530

    fix: Add release configuration file

---
+++ .github/files/release.config.js
@@ -0,0 +1,53 @@
+module.exports = {
+  branches: ["main", "dev/env"],
+  tagFormat: "${version}",
+  plugins: [
+    [
+      "@semantic-release/commit-analyzer",
+      {
+        preset: "angular",
+        parserOpts: {
+          noteKeywords: ["BREAKING CHANGE", "BREAKING CHANGES", "BREAKING"],
+        },
+      },
+    ],
+    [
+      "@semantic-release/release-notes-generator",
+      {
+        preset: "angular",
+        parserOpts: {
+          noteKeywords: ["BREAKING CHANGE", "BREAKING CHANGES", "BREAKING"],
+        },
+        writerOpts: {
+          commitsSort: ["subject", "scope"],
+        },
+      },
+    ],
+    [
+      "@semantic-release/changelog",
+      {
+        changelogFile: "CHANGELOG.md",
+      },
+    ],
+    [
+      "@semantic-release/exec",
+      {
+        prepareCmd:
+          'sed -i \'s/^version = ":.*/version = "${nextRelease.version}"/g\' build.gradle.kts',
+      },
+    ],
+    [
+      "@semantic-release/git",
+      {
+        assets: [
+          "CHANGELOG.md",
+          ".github/files/package-lock.json",
+          "build.gradle.kts",
+        ],
+        message:
+          "chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}",
+      },
+    ],
+    ["@semantic-release/github"],
+  ],
+};
