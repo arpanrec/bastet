4c680cdcd52cf3be2f177c7ef8de6902291dc3e1
arpan <arpan.rec@gmail.com>
2024-01-23 20:01:15 +0530

    Add GnuPG encryption to contextLoads() method

--- src/test/java/com/arpanrec/minerva/test/ApplicationTests.java
+++ src/test/java/com/arpanrec/minerva/test/ApplicationTests.java
@@ -1,12 +1,20 @@
 package com.arpanrec.minerva.test;
 
+import com.arpanrec.minerva.gnupg.GnuPG;
+import groovy.util.logging.Slf4j;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class ApplicationTests {
 
+    @Autowired
+    GnuPG gpg;
+
     @Test
-    void contextLoads() {}
+    void contextLoads() {
+        System.out.println(gpg.encrypt("Hello World!"));
+    }
 
 }
