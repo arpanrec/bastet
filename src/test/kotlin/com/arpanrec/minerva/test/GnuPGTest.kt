a24b0eb189898c8c05d9653f27ca6433b413dfa2
arpan <arpan.rec@gmail.com>
2024-01-25 16:51:49 +0530

    Update GnuPG configuration and add test cases

--- src/test/kotlin/com/arpanrec/minerva/test/GnuPGTest.kt
+++ src/test/kotlin/com/arpanrec/minerva/test/GnuPGTest.kt
@@ -1,20 +1,67 @@
 package com.arpanrec.minerva.test
 
 import com.arpanrec.minerva.gnupg.GnuPG
-import org.bouncycastle.openpgp.PGPException
 import org.junit.jupiter.api.Test
+import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
-import java.io.IOException
 
 @SpringBootTest
 class GnuPGTest {
+    private val log = LoggerFactory.getLogger(this.javaClass)
+    val message = "Hello, World!"
+    val armoredBcEncryptedMessage = """-----BEGIN PGP MESSAGE-----
+Version: BCPG v1.77.00
+
+wV4DubSNXBIhRMMSAQdAMzXed6cKZmK5X/PkUavSUSXUyxfCqcCs1kYUhGxRDkQw
+ojse2dwnw/9aPptGoD2aVzlLkSVQ0lxpkjLeQrVUlHCMnjhyAMeoqce4mvsI0bxo
+0mIBNEyj/X0lby5nFUg0wRpEfQ4gwUX5wAy0NJPNBDHko1OE8tuyTk44sMeJAcDD
+bEfLuDjkxbJffFsILsiy4JukCOT2YOS1g2eop3Coc+QGbpdVNfmbj2YB5m46ZiJL
+7jndTQ==
+=XQSg
+-----END PGP MESSAGE-----
+
+"""
+    val armoredCliEncryptedMessage = """-----BEGIN PGP MESSAGE-----
+
+hF4DubSNXBIhRMMSAQdAt9CNm3Bpm/jWLJhs8CKTnw13kV7m3hgzFw0AbuZqBlow
+MMwyyeFE92pUNljB6OW7ayl1gKkw4K6b2GK+AckTHZTKG7VTihSV/p/WN6Z1/mba
+1FsBCQIQyoMaLr6mnJ5FJsbCOOG24MM6etvVN8RK88M37DXEsc2RAe+b+O7PBjib
+GVD/xf7huNGhmdOr8OLf1X7jmzpUdEZLblbZ0fFR9V68E59oU6TT87nt4NVc
+=R8qx
+-----END PGP MESSAGE-----
+
+"""
+
     @Autowired
-    private var gnuPG: GnuPG? = null
+    private val gnuPG: GnuPG? = null
 
     @Test
-    @Throws(IOException::class, PGPException::class)
     fun testEncrypt() {
-        println(gnuPG!!.encrypt("Hello World!"))
+        val encryptedMessage = gnuPG!!.encrypt(message)
+        log.info("Encrypted message: {}", encryptedMessage)
+    }
+
+    @Test
+    fun testDecrypt() {
+        val decryptedMessage = gnuPG!!.decrypt(armoredBcEncryptedMessage)
+        log.info("Decrypted message: {}", decryptedMessage)
+        assert(decryptedMessage == message)
+    }
+
+    @Test
+    fun testEncryptAndDecrypt() {
+        val encryptedMessage = gnuPG!!.encrypt(message)
+        log.info("Encrypted message: {}", encryptedMessage)
+        val decryptedMessage = gnuPG.decrypt(encryptedMessage)
+        log.info("Decrypted message: {}", decryptedMessage)
+        assert(decryptedMessage == message)
+    }
+
+    @Test
+    fun testCliEncryptedMessage() {
+        val decryptedMessage = gnuPG!!.decrypt(armoredCliEncryptedMessage)
+        log.info("Decrypted message: {}", decryptedMessage)
+        assert(decryptedMessage == message)
     }
 }
