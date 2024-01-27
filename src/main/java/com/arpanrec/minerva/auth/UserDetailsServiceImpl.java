683b8b66976d0153b7d45ee01c54f81429d4935b
arpan <arpan.rec@gmail.com>
2024-01-28 03:11:03 +0530

    Refactor user serialization in UserDetailsServiceImpl

--- src/main/java/com/arpanrec/minerva/auth/UserDetailsServiceImpl.java
+++ src/main/java/com/arpanrec/minerva/auth/UserDetailsServiceImpl.java
@@ -3,8 +3,6 @@ package com.arpanrec.minerva.auth;
 import com.arpanrec.minerva.exceptions.MinervaException;
 import com.arpanrec.minerva.physical.KeyValue;
 import com.arpanrec.minerva.physical.KeyValuePersistence;
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.Getter;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,16 +10,20 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.util.Base64;
 import java.util.HashMap;
 import java.util.Optional;
 
+@Getter
 @Slf4j
 @Service
 public class UserDetailsServiceImpl implements UserDetailsService {
 
-    private final ObjectMapper mapper = new ObjectMapper();
-
-    @Getter
     private final KeyValuePersistence keyValuePersistence;
 
     public UserDetailsServiceImpl(@Autowired KeyValuePersistence keyValuePersistence) {
@@ -35,18 +37,23 @@ public class UserDetailsServiceImpl implements UserDetailsService {
         log.debug("Loading user by username: {}", username);
         Optional<KeyValue> userData = keyValuePersistence.get(USER_KEY_PREFIX + "/" + username, 0);
         userData.orElseThrow(() -> new UsernameNotFoundException("User not found"));
-        try {
-            return mapper.readValue(userData.get().getValue(), User.class);
-        } catch (JsonProcessingException e) {
+        byte[] data = Base64.getDecoder().decode(userData.get().getValue().getBytes());
+        try (ByteArrayInputStream byteArrayInputStream = new ByteArrayInputStream(data);
+             ObjectInputStream objectInputStream = new ObjectInputStream(byteArrayInputStream)) {
+            return (User) objectInputStream.readObject();
+        } catch (IOException e) {
             throw new UsernameNotFoundException("Error while loading user", e);
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException(e);
         }
     }
 
     public void saveUser(User user) throws MinervaException {
         log.debug("Saving user: {}", user);
-        try {
-            String userDataString = mapper.writeValueAsString(user);
-            KeyValue userData = new KeyValue(USER_KEY_PREFIX + "/" + user.getUsername(), userDataString, false, new HashMap<>(), 0);
+        try (ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream(); ObjectOutputStream objectOutputStream = new ObjectOutputStream(byteArrayOutputStream)) {
+            objectOutputStream.writeObject(user);
+            objectOutputStream.flush();
+            KeyValue userData = new KeyValue(USER_KEY_PREFIX + "/" + user.getUsername(), Base64.getEncoder().encodeToString(byteArrayOutputStream.toByteArray()), false, new HashMap<>(), 0);
             keyValuePersistence.save(userData);
         } catch (Exception e) {
             throw new MinervaException("Error while saving user", e);
