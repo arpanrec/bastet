2b9e6b2b7487c5fb9b770ece20cfcfa36774d0eb
arpan <arpan.rec@gmail.com>
2024-01-28 00:01:29 +0530

    Add GnuPG encryption and decryption to KeyValuePersistence

--- src/main/kotlin/com/arpanrec/minerva/physical/KeyValuePersistence.kt
+++ src/main/kotlin/com/arpanrec/minerva/physical/KeyValuePersistence.kt
@@ -1,5 +1,6 @@
 package com.arpanrec.minerva.physical
 
+import com.arpanrec.minerva.gnupg.GnuPG
 import jakarta.annotation.PostConstruct
 import java.nio.file.Path
 import java.util.Optional
@@ -10,13 +11,13 @@ import org.springframework.context.ApplicationContext
 
 @Serializable
 data class KeyValue(
-    var key: String? = null, var value: String? = null, var isBinary: Boolean = false, var metadata: Map<String, String> = mapOf(), var version: Int = 0
+    var key: String? = null, var value: String, var isBinary: Boolean = false, var metadata: Map<String, String> = mapOf(), var version: Int = 0
 )
 
 interface KeyValueStorage {
     fun save(keyValue: KeyValue): KeyValue
     fun update(keyValue: KeyValue): KeyValue
-    fun get(key: String, version: Int): Optional<KeyValue>
+    fun get(key: String, version: Int = 0): Optional<KeyValue>
     fun delete(keyValue: KeyValue): KeyValue
     fun getNextVersion(keyPath: Path): Int
     fun getLatestVersion(keyPath: Path): Int
@@ -27,6 +28,9 @@ class KeyValuePersistence(private var persistenceType: KeyValuePersistenceType) 
 
     @Autowired
     private var applicationContext: ApplicationContext? = null
+
+    @Autowired
+    private var gnuPG: GnuPG? = null
 
     private var keyValueStorage: KeyValueStorage? = null
 
@@ -39,11 +43,16 @@ class KeyValuePersistence(private var persistenceType: KeyValuePersistenceType) 
         }
     }
 
-    fun get(key: String, version: Int): Optional<KeyValue> {
-        return keyValueStorage!!.get(key = key, version = version)
+    fun get(key: String, version: Int = 0): Optional<KeyValue> {
+        val keyValue = keyValueStorage!!.get(key = key, version = version)
+        keyValue.ifPresent { keyValuePresent: KeyValue ->
+            keyValuePresent.value = gnuPG!!.decrypt(keyValuePresent.value)
+        }
+        return keyValue
     }
 
     fun save(keyValue: KeyValue): KeyValue {
+        keyValue.value = gnuPG!!.encrypt(keyValue.value)
         return keyValueStorage!!.save(keyValue)
     }
 }
