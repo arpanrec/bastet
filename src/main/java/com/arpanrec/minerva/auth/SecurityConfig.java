519d87bdb47fcf1282d6f5a6c5588321ae83f7ee
arpan <arpan.rec@gmail.com>
2024-01-30 18:50:11 +0530

    Update application.yml with port and context-path changes

--- src/main/java/com/arpanrec/minerva/auth/SecurityConfig.java
+++ src/main/java/com/arpanrec/minerva/auth/SecurityConfig.java
@@ -32,9 +32,11 @@ public class SecurityConfig {
 
     private final UserDetailsServiceImpl userDetailsServiceImpl;
 
-    public SecurityConfig(@Autowired AuthReqFilter authReqFilter, @Autowired AuthProvider authProvider,
+    public SecurityConfig(@Autowired AuthReqFilter authReqFilter,
+                          @Autowired AuthProvider authProvider,
                           @Autowired UserDetailsServiceImpl userDetailsServiceImpl,
-                          @Value("${minerva.auth.security-config.root-username:root}") String rootUsername, @Value("${minerva.auth.security-config.root-password:root}") String rootPassword) {
+                          @Value("${minerva.auth.security-config.root-username:root}") String rootUsername,
+                          @Value("${minerva.auth.security-config.root-password:root}") String rootPassword) {
         this.authReqFilter = authReqFilter;
         this.authProvider = authProvider;
         this.userDetailsServiceImpl = userDetailsServiceImpl;
@@ -44,8 +46,7 @@ public class SecurityConfig {
     }
 
     private RequestMatcher[] getPermitAllRequestMatchers() {
-        return new RequestMatcher[]{
-            new AntPathRequestMatcher("/h2-console/**"),
+        return new RequestMatcher[]{new AntPathRequestMatcher("/h2-console/**"),
             new AntPathRequestMatcher("/error")};
     }
 
@@ -54,18 +55,20 @@ public class SecurityConfig {
 
         http.cors(AbstractHttpConfigurer::disable);
         http.csrf(AbstractHttpConfigurer::disable);
-        http.headers(headers -> headers.frameOptions(HeadersConfigurer.FrameOptionsConfig::disable));
-        http.sessionManagement(
-            sessionManagement -> sessionManagement.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+        http.headers(
+            headers -> headers.frameOptions(HeadersConfigurer.FrameOptionsConfig::disable));
+        http.sessionManagement(sessionManagement -> sessionManagement
+            .sessionCreationPolicy(SessionCreationPolicy.STATELESS));
         http.authenticationProvider(authProvider);
         http.addFilterAfter(authReqFilter, BasicAuthenticationFilter.class);
 
-        http.authorizeHttpRequests(authorizeRequests -> authorizeRequests.requestMatchers(getPermitAllRequestMatchers()).permitAll());
         http.authorizeHttpRequests(authorizeRequests -> authorizeRequests
-            .requestMatchers(new AntPathRequestMatcher("/api/v1/keyvalue/internal/**")).hasAuthority(
-                User.Role.Type.ROLE_ADMIN.name())
-        );
-        http.authorizeHttpRequests(authorizeRequests -> authorizeRequests.anyRequest().authenticated());
+            .requestMatchers(getPermitAllRequestMatchers()).permitAll());
+        http.authorizeHttpRequests(authorizeRequests -> authorizeRequests
+            .requestMatchers(new AntPathRequestMatcher("/api/v1/keyvalue/internal/**"))
+            .hasAuthority(User.Role.Type.ROLE_ADMIN.name()));
+        http.authorizeHttpRequests(
+            authorizeRequests -> authorizeRequests.anyRequest().authenticated());
 
         // http.authorizeHttpRequests(authorizeRequests -> authorizeRequests.requestMatchers(new AntPathRequestMatcher("/**")).permitAll());
 
@@ -73,25 +76,24 @@ public class SecurityConfig {
     }
 
     private void doRootUserSetup() {
-        List<User.Privilege> rootPrivileges = List.of(new User.Privilege(User.Privilege.Type.SUDO.name()));
-        List<User.Role> rootRoles = List.of(
-            new User.Role(User.Role.Type.ROLE_ADMIN.name(), rootPrivileges),
-            new User.Role(User.Role.Type.ROLE_ANONYMOUS.name(), rootPrivileges),
-            new User.Role(User.Role.Type.ROLE_USER.name(), rootPrivileges)
-        );
-        User rootUser = User.builder().username(this.rootUsername).password(this.rootPassword).accountNonExpired(true)
-            .accountNonLocked(true).credentialsNonExpired(true).enabled(true).roles(rootRoles).build();
-        userDetailsServiceImpl.getKeyValuePersistence().get(
-                userDetailsServiceImpl.getInternalUsersKeyPath() + "/" + rootUsername, 0)
-            .ifPresentOrElse(
-                (kv) -> log.info("Root user already exists"),
-                () -> {
-                    try {
-                        userDetailsServiceImpl.saveUser(rootUser);
-                        log.info("Root user created");
-                    } catch (Exception e) {
-                        throw new RuntimeException("Error while creating root user", e);
-                    }
-                });
+        List<User.Privilege> rootPrivileges =
+            List.of(new User.Privilege(User.Privilege.Type.SUDO.name()));
+        List<User.Role> rootRoles =
+            List.of(new User.Role(User.Role.Type.ROLE_ADMIN.name(), rootPrivileges),
+                new User.Role(User.Role.Type.ROLE_ANONYMOUS.name(), rootPrivileges),
+                new User.Role(User.Role.Type.ROLE_USER.name(), rootPrivileges));
+        User rootUser = User.builder().username(this.rootUsername).password(this.rootPassword)
+            .accountNonExpired(true).accountNonLocked(true).credentialsNonExpired(true)
+            .enabled(true).roles(rootRoles).build();
+        userDetailsServiceImpl.getKeyValuePersistence()
+            .get(userDetailsServiceImpl.getInternalUsersKeyPath() + "/" + rootUsername, 0)
+            .ifPresentOrElse((kv) -> log.info("Root user already exists"), () -> {
+                try {
+                    userDetailsServiceImpl.saveUser(rootUser);
+                    log.info("Root user created");
+                } catch (Exception e) {
+                    throw new RuntimeException("Error while creating root user", e);
+                }
+            });
     }
 }
