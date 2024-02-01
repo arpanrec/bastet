b0597eee9e438e3117d5f9c2fdb72e1262268f50
arpan <arpan.rec@gmail.com>
2024-02-01 22:28:08 +0530

    Refactor KeyValueTests to include additional test cases

--- src/test/kotlin/com/arpanrec/minerva/test/physical/KeyValueTests.kt
+++ src/test/kotlin/com/arpanrec/minerva/test/physical/KeyValueTests.kt
@@ -15,11 +15,42 @@ class KeyValueTests(@Autowired private val keyValuePersistence: KeyValuePersiste
 
     @Test
     fun testSave() {
-        val keyValue = KeyValue("key", "1")
-        keyValuePersistence.save(keyValue)
-        val keyValue2 = keyValuePersistence.get("key")
-        if (keyValue2.isPresent) {
-            log.info("keyValue2: {}", keyValue2.get())
+        val keyValueSave = KeyValue("testSave", "1")
+        keyValuePersistence.save(keyValueSave)
+        val keyValueGet = keyValuePersistence.get("testSave")
+        if (keyValueGet.isPresent) {
+            log.info("keyValueGet: {}", keyValueGet.get())
+            assert(keyValueGet.get().key == keyValueSave.key) { "keyValueSave.key is not equal to keyValueGet.key" }
+        } else {
+            assert(false) { "keyValue2 is null" }
+        }
+    }
+
+    @Test
+    fun testSaveVersion() {
+        val keyValueSave = KeyValue("testSaveVersion", "1")
+        keyValuePersistence.save(keyValueSave)
+        val keyValueSaveNextVersion = KeyValue("testSaveVersion", "2")
+        keyValuePersistence.update(keyValueSaveNextVersion)
+        val keyValueGet = keyValuePersistence.get("testSaveVersion")
+        if (keyValueGet.isPresent) {
+            log.info("keyValueGet: {}", keyValueGet.get())
+            assert(keyValueGet.get().key == keyValueSaveNextVersion.key) {
+                "keyValueGet.key is not equal to keyValueSaveNextVersion.key"
+            }
+        } else {
+            assert(false) { "keyValue2 is null" }
+        }
+
+        val keyValueGetOldVersion = keyValuePersistence.get("testSaveVersion", 1)
+
+        if (keyValueGetOldVersion.isPresent) {
+            log.info("keyValueGetOldVersion: {}", keyValueGetOldVersion.get())
+            assert(keyValueGetOldVersion.get().key == keyValueSave.key) {
+                "keyValueGetOldVersion.key is not equal to keyValueSave.key"
+            }
+        } else {
+            assert(false) { "keyValueGetOldVersion is null" }
         }
     }
 }
