80d0a3b1914058bcfc8c8d17955264b6ccd44000
arpan <arpan.rec@gmail.com>
2024-02-01 20:10:46 +0530

    Add .fleet/settings.json and enable restart in devtools

--- src/main/java/com/arpanrec/minerva/auth/MinervaUserDetails.java
+++ src/main/java/com/arpanrec/minerva/auth/MinervaUserDetails.java
@@ -3,7 +3,6 @@ package com.arpanrec.minerva.auth;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonProperty;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.Getter;
