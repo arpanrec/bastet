7354b91904574de11cce997c4a7758e933c9b4b3
arpan <arpan.rec@gmail.com>
2024-02-04 00:36:36 +0530

    Refactor KeyValueStorage interface

--- src/main/kotlin/com/arpanrec/minerva/physical/KeyValueStorage.kt
+++ src/main/kotlin/com/arpanrec/minerva/physical/KeyValueStorage.kt
@@ -4,11 +4,26 @@ import java.nio.file.Path
 import java.util.Optional
 
 interface KeyValueStorage {
+
     fun save(keyValue: KeyValue): KeyValue
+
     fun update(keyValue: KeyValue): KeyValue
-    fun get(key: String, version: Int = 0): Optional<KeyValue>
-    fun delete(keyValue: KeyValue, version: Int = 0): KeyValue
+
+    fun get(key: String): Optional<KeyValue>
+
+    fun get(key: String, version: Int): Optional<KeyValue>
+
+    fun delete(keyValue: KeyValue, version: Int): Boolean
+
+    fun delete(keyValue: KeyValue): Boolean
+
+    fun deleteAllVersions(keyValue: KeyValue): Boolean
+
     fun getNextVersion(keyPath: Path): Int
+
     fun getLatestVersion(keyPath: Path): Int
+
+    fun listVersions(keyPath: Path): List<Int>
+
     fun listKeys(keyPath: Path): List<String>
 }
