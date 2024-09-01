b99f91fad16a14f3c3cb39b1fc6758216939a12b
arpan <arpan.rec@gmail.com>
2024-09-02 02:22:07 +0530

    wip

--- src/main/kotlin/com/arpanrec/minerva/physical/KeyValuePersistence.kt
+++ src/main/kotlin/com/arpanrec/minerva/physical/KeyValuePersistence.kt
@@ -9,8 +9,6 @@ import java.util.Optional
 class KeyValuePersistence(
     @Autowired private val gnuPG: GnuPG
 ) {
-
-
     var internalStorageKey: String = "internal"
 
     override fun get(key: String): Optional<KVData> {
