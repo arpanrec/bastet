6e714b225e8b99f7c379bf046a38428817e5350e
arpan <arpan.rec@gmail.com>
2024-09-06 02:45:58 +0530

    wip

--- src/main/kotlin/com/arpanrec/minerva/physical/KVData.kt
+++ src/main/kotlin/com/arpanrec/minerva/physical/KVData.kt
@@ -4,6 +4,7 @@ import jakarta.persistence.Column
 import jakarta.persistence.Convert
 import jakarta.persistence.Entity
 import jakarta.persistence.Id
+import jakarta.persistence.Table
 import kotlinx.serialization.Serializable
 import lombok.AllArgsConstructor
 import lombok.Data
@@ -12,6 +13,7 @@ import lombok.NoArgsConstructor
 @Entity
 @Serializable
 // @Table(name = "kv_data", uniqueConstraints = [UniqueConstraint(columnNames = ["id", "version"])])
+@Table(name = "kv_data")
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
