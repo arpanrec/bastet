02743d35290fd4ead5201e9bbdd80abb17bac89c
arpan <arpan.rec@gmail.com>
2024-09-06 02:41:37 +0530

    wip

--- src/main/java/com/arpanrec/minerva/state/LoggingConfiguration.java
+++ src/main/java/com/arpanrec/minerva/state/LoggingConfiguration.java
@@ -1,12 +1,21 @@
 package com.arpanrec.minerva.state;
 
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class LoggingConfiguration {
+
+    //    @Bean
+//    public RequestAndResponseLoggingFilter requestResponseLoggingFilter() {
+//        return new RequestAndResponseLoggingFilter();
+//    }
     @Bean
-    public RequestAndResponseLoggingFilter requestResponseLoggingFilter() {
-        return new RequestAndResponseLoggingFilter();
+    public FilterRegistrationBean<RequestAndResponseLoggingFilter> loggingFilter() {
+        FilterRegistrationBean<RequestAndResponseLoggingFilter> registrationBean = new FilterRegistrationBean<>();
+        registrationBean.setFilter(new RequestAndResponseLoggingFilter());
+        registrationBean.addUrlPatterns("/*");
+        return registrationBean;
     }
 }
