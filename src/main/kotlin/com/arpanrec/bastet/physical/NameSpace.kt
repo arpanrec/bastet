7bb25fde4454fb84706e7978a6723bafac6f3728
arpan <arpan.rec@gmail.com>
2024-09-08 03:01:03 +0530

    Wip

--- src/main/kotlin/com/arpanrec/bastet/physical/NameSpace.kt
+++ src/main/kotlin/com/arpanrec/bastet/physical/NameSpace.kt
@@ -4,9 +4,8 @@ class NameSpace {
     companion object {
         private const val INTERNAL = "/internal"
         const val INTERNAL_ARGON2 = "$INTERNAL/argon2"
-        const val INTERNAL_TF_STATE = "$INTERNAL/tf_state/"
+        const val INTERNAL_TF_STATE = "$INTERNAL/tf_state"
         const val INTERNAL_USERS = "$INTERNAL/users"
-        const val INTERNAL_AES256CBC = "$INTERNAL/aes256cbc"
         const val INTERNAL_GNUPG = "$INTERNAL/gnupg"
     }
 }
